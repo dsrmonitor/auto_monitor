f55202e0a6da68668e390b2dcca1b64bba87085e
dsrmonitor <dscontrol.geral@gmail.com>
2018-07-08 17:48:26 -0300

    Tratamento para decodificar as mensagens de posicao chegadas do rastreador

---
+++ srv_receive_data/Repository/Entities/log_vehicle_position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+    public class log_vehicle_position
+    {
+        public virtual int id { get; set; }
+        public virtual int vehicle_id { get; set; }
+        public virtual string west { get; set; }
+        public virtual string south { get; set; }
+        public virtual float speed { get; set; }
+        public virtual DateTime timestamp { get; set; }
+
+    }
+}
---
+++ srv_receive_data/Repository/Mapping/log_vehicle_positionMap.cs
@@ -0,0 +1,24 @@
+﻿using FluentNHibernate.Mapping;
+using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Mapping
+{
+    public class log_vehicle_positionMap: ClassMap<log_vehicle_position>
+    {
+        public log_vehicle_positionMap()
+        {
+            Id(c => c.id);
+            Map(c => c.vehicle_id);
+            Map(c => c.west);
+            Map(c => c.south);
+            Map(c => c.speed);
+            Map(c => c.timestamp);
+            Table("log_vehicle_position");
+        }
+    }
+}
